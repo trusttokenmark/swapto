806d2326dde3cbdc09659e0df1fc2bfa4af95562
Mark Mironyuk <mark.mironyuk@trusttoken.com>
2019-05-09 14:50:22 -0700

    Build

---
+++ docs/precache-manifest.08707be4f8c1877d0000ec73fa2c22bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcd469bfb29dd769d3205603bee64a2",
+    "url": "/swapto/index.html"
+  },
+  {
+    "revision": "861eebd9d8032961b2a8",
+    "url": "/swapto/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/swapto/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "861eebd9d8032961b2a8",
+    "url": "/swapto/static/js/main.86ca790a.chunk.js"
+  },
+  {
+    "revision": "f3cee6990f26ef29b976",
+    "url": "/swapto/static/js/runtime~main.6d063132.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/swapto/static/media/logo.5d5d9eef.svg"
+  }
+]);
